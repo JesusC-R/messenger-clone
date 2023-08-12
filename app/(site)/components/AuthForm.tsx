ae6a7d08cc555eb890843e3ed471f03c2788c1c1
JesusC-R <cuevasrodartej@gmail.com>
2023-08-11 22:09:32 -0700

    Feat: Add SignIn form

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { useState, useCallback } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+
+import Input from "@/app/components/input/Input";
+import Button from "@/app/components/button";
+
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      // Axios register
+    }
+
+    if (variant === "LOGIN") {
+      // NextAuth Sign In
+    }
+  };
+
+  const socialAction = () => {
+    setIsLoading(true);
+
+    // NextAuth Social Sign In
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+          <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email Address"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <Button
+              disabled={isLoading}
+              fullWidth
+              type="submit"
+            >
+              {variant === "LOGIN" ? "Sign In" : "Register"}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
