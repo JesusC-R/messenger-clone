b7e5e0b2a08f8f2c04c828fe5b491b7c393a01d3
JesusC-R <cuevasrodartej@gmail.com>
2023-08-15 20:10:32 -0700

    Feat: Add AuthContext component to retrive session

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import ToasterContext from "./context/toaster-context";
+import AuthContext from "./context/auth-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <ToasterContext />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
+      </body>
     </html>
   );
 }
