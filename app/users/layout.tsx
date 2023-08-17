41b7fbc308c430f1a9a9bf50f6b9e20441309dc9
JesusC-R <cuevasrodartej@gmail.com>
2023-08-16 19:23:59 -0700

    Feat: Add Navbar.

---
+++ app/users/layout.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "@/app/components/sidebar/sidebar";
+
+export default async function UsersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Sidebar>
+      <div className="h-full">
+        {children}
+      </div>
+    </Sidebar>
+  );
+}
