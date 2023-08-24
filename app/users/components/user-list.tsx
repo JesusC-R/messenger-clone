53cfd6046d606d1eafe21d9378393dc64542a794
JesusC-R <cuevasrodartej@gmail.com>
2023-08-23 22:40:14 -0700

    Fix: Fix class type

--- app/users/components/user-list.tsx
+++ app/users/components/user-list.tsx
@@ -13,7 +13,7 @@ const UserList: React.FC<UserListProps> = ({ items }) => {
     <aside
       className="fixed inset-y-0 pb-20 lg:pb-0
         lg:left-20 lg:w-80 lg:block overflow-y-auto
-        border-r border-gray-200 block w-full left-0"
+        border-r border-gray-200 block w-full left-0 sm:hidden"
     >
       <div className="px-5">
         <div className="flex-col">
