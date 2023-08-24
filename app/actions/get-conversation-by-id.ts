0266b72135df25ae06aca15b0778832cda7073aa
JesusC-R <cuevasrodartej@gmail.com>
2023-08-23 21:28:21 -0700

    Feat: Add conversation data fetching

---
+++ app/actions/get-conversation-by-id.ts
@@ -0,0 +1,28 @@
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "./get-current-user";
+import conversationId from "../conversations/[conversationId]/page";
+
+const getConversationById = async (conversationId: string) => {
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser?.email) {
+      return null;
+    }
+
+    const conversation = await prisma.conversation.findUnique({
+      where: {
+        id: conversationId,
+      },
+      include: {
+        users: true,
+      },
+    });
+
+    return conversation;
+  } catch (err: any) {
+    return null;
+  }
+};
+
+export default getConversationById;
